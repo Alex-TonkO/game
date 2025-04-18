a47d1b4505acf5f8d3d4f25665cbd2eae67ecf49
Tonko_wq55 <awdawd@gaawd>
2025-04-18 12:17:53 +0300

    3

--- tic_tac.py
+++ tic_tac.py
@@ -2,10 +2,11 @@ import random
 import tkinter as tk
 from tkinter import messagebox
 
+
 class TicTacToe:
     def __init__(self, root):
         self.root = root
-        self.root.title ("Tic Tac Toe")
+        self.root.title("Tic Tac Toe")
         self.player = "X"
         self.comp = "0"
         self.board = [""] * 9
@@ -15,10 +16,16 @@ class TicTacToe:
     def create_board(self):
         for i in range(9):
             button = tk.Button(self.root, text="", font=("Arial, 40"), width=5, height=2,
-                               command=lambda i=i:self.player_move(i))
+                               command=lambda i=i: self.player_move(i))
+            button.grid(row=i // 3, column=i % 3)
+            self.buttons.append(button)
+
+    def player_move(self, index):
+        if self.board[index] == "":
+            self.board[index] = self.player
 
 
 if __name__ == "__main__":
-        root = tk.Tk()
-        game = TicTacToe(root)
-        root.mainloop()
+    root = tk.Tk()
+    game = TicTacToe(root)
+    root.mainloop()
